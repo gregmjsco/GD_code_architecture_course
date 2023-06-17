ed33d19d20f94d72f3c3097f7d1c85f4b0aa031f
Greg Johnston <gregmj.sco@gmail.com>
2023-06-17 14:55:37 +0900

    Add forward player movement to W key

--- player.gd
+++ player.gd
@@ -8,6 +8,21 @@ func _ready():
 
 
 func _input(event):
+	aim(event)
+	
+
+func _physics_process(delta):
+	var movement_vector: Vector3
+	var forward_movement: Vector3
+	
+	if Input.is_action_pressed("move_forward"):
+		forward_movement = -transform.basis.z
+		
+	movement_vector = forward_movement
+	velocity = movement_vector
+	move_and_slide()
+
+func aim(event: InputEvent) -> void: 
 	var mouse_motion = event as InputEventMouseMotion
 	if mouse_motion:
 		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
@@ -16,7 +31,7 @@ func _input(event):
 		current_tilt -= mouse_motion.relative.y * _mouse_sensitivity
 		
 		$Camera3D.rotation_degrees.x = clamp(current_tilt, -90, 90)
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
