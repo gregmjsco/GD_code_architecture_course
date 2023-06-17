c2da272ae6e8b4ed10db34069225bdc3d097445e
Greg Johnston <gregmj.sco@gmail.com>
2023-06-17 14:21:14 +0900

    Add player mouse camera

--- player.gd
+++ player.gd
@@ -11,6 +11,11 @@ func _input(event):
 	var mouse_motion = event as InputEventMouseMotion
 	if mouse_motion:
 		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
+		
+		var current_tilt: float = $Camera3D.rotation_degrees.x     
+		current_tilt -= mouse_motion.relative.y * _mouse_sensitivity
+		
+		$Camera3D.rotation_degrees.x = clamp(current_tilt, -90, 90)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
